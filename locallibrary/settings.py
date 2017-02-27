45b949b128a47b25fa879dc63c3e3d9a5d991f67
byronvhughey <byron.v.hughey@gmail.com>
2017-02-27 17:24:04 -0500

    moved settings.py over

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
-SECRET_KEY = is.environ.get('DJANGO_SECRET_KEY', '$waceh(xn5wn-o(fxg5qh4!$yrx=kpt@@8_o#1*)1pqr-z&e6(')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '$waceh(xn5wn-o(fxg5qh4!$yrx=kpt@@8_o#1*)1pqr-z&e6(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUT = bool( os.environ.get('DJANGO_DEBUG', True))
+DEBUT = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
@@ -130,3 +130,26 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files
+
+# The absolute path to the directory where collectstatic will 
+# collect static files for deployment
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFileStorage'
+
+
+
